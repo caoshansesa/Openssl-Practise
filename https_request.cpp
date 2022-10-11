5649244f981569e086d25e5b05446103cf4882e3
Your Name <you@example.com>
2022-10-10 21:45:59 -0400

    This version can get and parse server cert

--- https_request.cpp
+++ https_request.cpp
@@ -1,114 +1,116 @@
-/* filename withssl.c */
-#include "stdio.h"
-#include "string.h"
-
-#include "openssl/ssl.h"
-#include "openssl/bio.h"
-#include "openssl/err.h"
-
-int main(int argc, char *argv[])
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#define FAIL    -1
+int OpenConnection(const char *hostname, int port)
 {
-    BIO * bio;
-    SSL * ssl;
-    SSL_CTX * ctx;
-
-    char resp[1024];
-    int  ret;
-
-    char* request = "GET /v1/pki_int_D8C0F20133/crl HTTP/1.1\x0D\x0A http://10.169.37.248:8200\x0D\x0A\x43onnection: Close\x0D\x0A\x0D\x0A";
-    //char * request = "GET / HTTP/1.1\x0D\x0AHost: worktile.com\x0D\x0A\x43onnection: Close\x0D\x0A\x0D\x0A";
-    //char * request = "GET /cas/login?service=https%3A%2F%2Fweb.corp.ema-tech.com%3A8888%2F HTTP/1.1\x0D\x0AHost: web.corp.ema-tech.com\x0D\x0A""Connection: Close\x0D\x0A\x0D\x0A";
-
-    /* Attention: "\x43" is "C", why is "\x43onnection" but not "Connection" ?
-       because "\x0AC" in "\x0D\x0AConnection" is taken as a hex value, not the string "\nConnection"
-       also, "\x0D""C" can avoid upper problem too.
-     */
-
-    /* Init SSL library */
-    SSL_library_init();
-
-    /* Set up the library */
-    ERR_load_BIO_strings();
-    SSL_load_error_strings();
-    OpenSSL_add_all_algorithms();
-
-    /* Set up the SSL context */
-    const SSL_METHOD *method = SSLv23_client_method();	/* SSLv3 but can rollback to v2 */
-    if (! method) {
-        fprintf(stderr, "SSL client method failed\n");
-        return 1;
-    }
-
-    ctx = SSL_CTX_new(method);
-    if (! ctx) {
-        fprintf(stderr, "SSL context is NULL\n");
-        ERR_print_errors_fp(stderr);
-        return 1;
-    }
-
-#ifdef NO_VERIFY
-    /* Load the trust store */
-    if(! SSL_CTX_load_verify_locations(ctx, argv[1], NULL)) {
-        fprintf(stderr, "Error loading trust store\n");
-        ERR_print_errors_fp(stderr);
-        SSL_CTX_free(ctx);
-        return 0;
-    }
-#endif
-
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL); // Once way verification
-
-    /* Setup the connection */
-    bio = BIO_new_ssl_connect(ctx);
-
-    /* Set the SSL_MODE_AUTO_RETRY flag */
-    BIO_get_ssl(bio, &ssl);
-    SSL_set_mode(ssl, SSL_MODE_AUTO_RETRY);
-
-    /* Create and setup the connection */
-    BIO_set_conn_hostname(bio, "10.169.37.248:8200");
-    //BIO_set_conn_hostname(bio, "worktile.com:https");
-    //BIO_set_conn_hostname(bio, "web.corp.ema-tech.com:8888");
-
-    if(BIO_do_connect(bio) <= 0) {
-        fprintf(stderr, "Error attempting to connect\n");
-        ERR_print_errors_fp(stderr);
-        BIO_free_all(bio);
-        SSL_CTX_free(ctx);
-        return 0;
-    }
-
-    /* Check the certificate */
-    if(SSL_get_verify_result(ssl) != X509_V_OK)
+    int sd;
+    struct hostent *host;
+    struct sockaddr_in addr;
+    if ( (host = gethostbyname(hostname)) == NULL )
     {
-        fprintf(stderr, "Certificate verification error: %ld\n", SSL_get_verify_result(ssl));
-        /*
-          Error Tip "error : 19 self signed certificate in certificate chain" means:
-          This means the certificate chain returned by the server ends with a ‘self signed certificate’.
-          Since the self-signed certificate is not a trusted certificate, it is reported as an error.
-          You can make the problem go away by specifying a trusted root CA (certificate authority)
-         */
-        fprintf(stderr, "Error: %s\n", ERR_reason_error_string(ERR_get_error()));
+        perror(hostname);
+        abort();
+    }
+    sd = socket(PF_INET, SOCK_STREAM, 0);
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = *(long*)(host->h_addr);
+    if ( connect(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
+    {
+        close(sd);
+        perror(hostname);
+        abort();
+    }
+    return sd;
+}
+SSL_CTX* InitCTX(void)
+{
+    SSL_METHOD *method;
+    SSL_CTX *ctx;
+    OpenSSL_add_all_algorithms();  /* Load cryptos, et.al. */
+    SSL_load_error_strings();   /* Bring in and register error messages */
+    method = TLSv1_2_client_method();  /* Create new client-method instance */
+    ctx = SSL_CTX_new(method);   /* Create new context */
+    if ( ctx == NULL )
+    {
         ERR_print_errors_fp(stderr);
-        BIO_free_all(bio);
-        SSL_CTX_free(ctx);
-        return 0;
+        abort();
     }
-
-    /* Send the request */
-    BIO_write(bio, request, strlen(request));
-
-    /* Read in the response */
-    for(;;) {
-        ret = BIO_read(bio, resp, 1023);
-        if(ret <= 0) break;
-        resp[ret] = 0;
-        printf("%s", resp);
+    return ctx;
+}
+void ShowCerts(SSL* ssl)
+{
+    X509 *cert;
+    char *line;
+    cert = SSL_get_peer_certificate(ssl); /* get the server's certificate */
+    if ( cert != NULL )
+    {
+        printf("Server certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        printf("Subject: %s\n", line);
+        free(line);       /* free the malloc'ed string */
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);       /* free the malloc'ed string */
+        X509_free(cert);     /* free the malloc'ed certificate copy */
     }
-
-    /* Close the connection and free the context */
-    BIO_free_all(bio);
-    SSL_CTX_free(ctx);
+    else
+        printf("Info: No client certificates configured.\n");
+}
+int main(int count, char *strings[])
+{
+    SSL_CTX *ctx;
+    int server;
+    SSL *ssl;
+    char buf[1024];
+    char acClientRequest[1024] = {0};
+    int bytes;
+    char *hostname, *portnum;
+    if ( count != 3 )
+    {
+        printf("usage: %s <hostname> <portnum>\n", strings[0]);
+        exit(0);
+    }
+    SSL_library_init();
+    hostname=strings[1];
+    portnum=strings[2];
+    ctx = InitCTX();
+    server = OpenConnection(hostname, atoi(portnum));
+    ssl = SSL_new(ctx);      /* create new SSL connection state */
+    SSL_set_fd(ssl, server);    /* attach the socket descriptor */
+    if ( SSL_connect(ssl) == FAIL )   /* perform the connection */
+        ERR_print_errors_fp(stderr);
+    else
+    {
+        char acUsername[16] = {0};
+        char acPassword[16] = {0};
+        const char *cpRequestMessage = "<Body>\
+                               <UserName>%s<UserName>\
+                 <Password>%s<Password>\
+                 <\Body>";
+        printf("Enter the User Name : ");
+        scanf("%s",acUsername);
+        printf("\n\nEnter the Password : ");
+        scanf("%s",acPassword);
+        sprintf(acClientRequest, cpRequestMessage, acUsername,acPassword);   /* construct reply */
+        printf("\n\nConnected with %s encryption\n", SSL_get_cipher(ssl));
+        ShowCerts(ssl);        /* get any certs */
+        SSL_write(ssl,acClientRequest, strlen(acClientRequest));   /* encrypt & send message */
+        bytes = SSL_read(ssl, buf, sizeof(buf)); /* get reply & decrypt */
+        buf[bytes] = 0;
+        printf("Received: \"%s\"\n", buf);
+        SSL_free(ssl);        /* release connection state */
+    }
+    close(server);         /* close socket */
+    SSL_CTX_free(ctx);        /* release context */
     return 0;
 }
-
