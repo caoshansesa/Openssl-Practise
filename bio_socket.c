ead2f0f4ff54ae7ae790c196d289ef2e52734970
Your Name <you@example.com>
2023-01-04 11:55:15 -0500

    wsl --install

--- bio_socket.c
+++ bio_socket.c
@@ -12,17 +12,31 @@ int main()
         ret = -1;
         printf("BIO creation failed.\n");
     }
-    BIO_set_conn_hostname(bio, "10.169.37.180");
-    BIO_set_conn_port(bio, "5000");
+    BIO_set_conn_hostname(bio, "10.169.37.248");
+    BIO_set_conn_port(bio, "80");
     ret = BIO_do_connect(bio);
 
-
     int num_bytes_written = 0;
-    char request[500] =
-        "GET  /PKIClient/api/crl/pki_int_953B9DA915 HTTP/1.1\r\n\0\0";
+    char request[128] = "GET  / HTTP/1.1\r\n";
 
     char response[1024] = "";
     BIO_puts(bio, request);
+
+    int num_bytes_read = 0;
+    do
+    {
+        num_bytes_read = BIO_read(bio, response, 1023);
+        if ((num_bytes_read < 1023) && (num_bytes_read > 0))
+        {
+            break; // read in size < 1024, buffer is empty.
+            response[1023] = '\0';
+            if (fputs(response, stdout) == EOF)
+            {
+                printf("\n Error : Fputs error\n");
+            }
+        }
+
+    } while (num_bytes_read > 0 || BIO_should_retry(bio));
     getchar();
     BIO_free(bio);
     return 0;
